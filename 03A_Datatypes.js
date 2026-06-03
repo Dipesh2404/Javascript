bfae9ca45e19e5d21da7c45160c99bea9619555b
Dipesh Choudhary <148421683+Dipesh2404@users.noreply.github.com>
2026-06-03 15:10:54 +0530

    Add files via upload

---
+++ 03A_Datatypes.js
@@ -0,0 +1,16 @@
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to the power 53
+// BigInt
+// strint => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+
+
+
+// Object
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
